6b2b3bd21c8479ead47e7eebebf134b79627035d
unknown <mohammednageh832@gmail.com>
2022-12-07 11:23:43 +0200

    Final touches

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-const clockEl = document.querySelector(".clock");
+const timerElement = document.querySelector(".timer");
 let hh = 0;
 let mm = 0;
 let ss = 0;
+let isRunning = true;
 
-setInterval(() => updateClock(), 1000);
+let timerInterval = setInterval(() => updateClock(), 1000);
+
+const toggleTimerBtn = document.querySelector(".toggle-timer");
+const resetTimerBtn = document.querySelector(".reset-timer");
+
+toggleTimerBtn.addEventListener("click", () => {
+  if (isRunning) {
+    stopTimer();
+    toggleTimerBtn.textContent = "Play";
+  } else {
+    playTimer();
+    toggleTimerBtn.textContent = "Stop";
+  }
+});
+
+resetTimerBtn.addEventListener("click", () => {
+  resetTimer();
+});
+
+/******************************************** Utility functions *******************************************/
 
 function updateClock() {
   if (ss > 59) {
@@ -16,9 +36,31 @@ function updateClock() {
 
   ss++;
 
+  timerElement.textContent = createClock();
+}
+
+function createClock() {
   const adjustedHH = hh < 10 ? `0${hh}` : hh;
   const adjustedMM = mm < 10 ? `0${mm}` : mm;
   const adjustedSS = ss < 10 ? `0${ss}` : ss;
 
-  clockEl.textContent = `${adjustedHH}:${adjustedMM}:${adjustedSS}`;
+  return `${adjustedHH}:${adjustedMM}:${adjustedSS}`;
+}
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  isRunning = false;
+}
+
+function playTimer() {
+  timerInterval = setInterval(() => updateClock(), 1000);
+  isRunning = true;
+}
+
+function resetTimer() {
+  hh = 0;
+  mm = 0;
+  ss = 0;
+
+  timerElement.textContent = createClock();
 }
